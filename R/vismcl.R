1e1ff03e4ae003b232ecda4ea42231e7a20a65bc
Liwen Zhuang <zhuangyedda@gmail.com>
2018-11-10 15:01:22 -0500

    fix size problem, delete useless files, create example folder in inst.

--- R/vismcl.R
+++ R/vismcl.R
@@ -88,8 +88,12 @@ AddToList <- function(filepath) {
 #' creat dataframe used for plotting from nested list
 #'
 #' Details.
+#' @export
 #' @param filepath filepath of the output of MCL
 #' @return a dataframe used for plotting.
+#' @examples
+#' cat("a aa\n",file = "A")
+#' CreatDataFrame("A")
 
 CreatDataFrame <- function(filepath) {
   Nested_list<-AddToList(filepath)
@@ -114,52 +118,76 @@ CreatDataFrame <- function(filepath) {
 #' plot circle packing graph
 #'
 #' @param filepath the filepath of the output file of MCL
-#' @param showName if shows the name of clusters,default is FALSE
-#' @param color A color code:color fill the circle,default is grey#'
+#' @param showName (optional)if shows the name of clusters,default is FALSE
+#' @param color (optional)A color code:color fill the circle,default is grey
+#' @param Clusternames (optional) a vector for user-defined clusternames,
+#' order should be the same as the lines go.
 #' @return a circle packing graph representing the information
 #' @export
 #' @import ggplot2
 #' @import ggiraph
 #' @import packcircles
 #' @examples
-#' cat("a aa\n",file = "A")
+#' cat("a aa\n b bbb b\n",file = "A")
 #' vismcl("A")
+#' vismcl("A",showName = TRUE,color = "red")
+#' Clust_names<-c("a","b")
+#' vismcl("A",showName = TRUE,Clusternames = Clust_names)
 
-vismcl <- function(filepath,showName=FALSE,color="grey") {
+vismcl <- function(filepath,showName=FALSE,Clusternames=NULL,color="grey") {
+  #for package checking
   x<- y<- id<-NULL
   input_df<-CreatDataFrame(filepath)
+
+  #if user provide Cluster name list,substitue it to the dataframe.
+  #but first error checking:
+  #(1):if more cluster names provided than acutual numbers of cluster, then definitly wrong, stop and produce error messages.
+  #(2):if not enough cluster names, produce a warning messege (in case user only intentionally do that,programe still produce the result),
+  #    use automatically formed name as substitution.
+  if (is.null(Clusternames) == FALSE){
+    if (length(Clusternames)!=nrow(input_df)){
+      if (length(Clusternames) > nrow(input_df)){
+        stop("More Cluster names than number of clusters")
+      }
+      else if (length(Clusternames) < nrow(input_df)){
+        warning("not enough cluster names provided, the rest will be substitue by automaticly formed names")
+        names<-sprintf("cluster%d",1:length(nrow(input_df)-length(Clusternames)))
+        Clusternames<-c(Clusternames,names)
+      }
+    }
+    input_df$Name<-Clusternames
+  }
+  print(input_df)
+
   #genearte dataframe for the position and size of each circle
-  packing <- circleProgressiveLayout(input_df)
+  packing <- circleProgressiveLayout(input_df$Area)
   dat.gg <- circleLayoutVertices(packing,npoints=50)
-  dat.gg
-  if (showName){
-    gg<-ggplot(data = dat.gg) +
+  gg<-ggplot(data = dat.gg) +
       geom_polygon_interactive(
-        aes(x, y, group = id,
-            tooltip = input_df$Element[id], data_id = id),
-        fill=color,
-        colour = "black",
-        show.legend = FALSE) +
-      geom_text(data=packing,aes(x,y),label= input_df$Name)+
-      scale_y_reverse() +
+      aes(x, y, group = id,tooltip = input_df$Element[id], data_id = id),
+      fill=color,
+      colour = "black",
+      show.legend = FALSE
+      ) +
       labs(title = "Hover over circle to display cluster elements") +
+      theme(axis.title.x=element_blank(),
+            axis.text.x=element_blank(),
+            axis.ticks.x=element_blank(),
+            axis.title.y=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank(),
+            panel.border = element_rect(colour = "black", fill=NA, size=2),
+            panel.background = element_blank()
+            )+
       coord_equal()
-    ggiraph(ggobj = gg, width_svg = 5, height_svg = 5)
+  #if user want to display the name of cluster
+  if(showName){
+  gg<-gg+geom_text(data=packing,aes(x,y),label= input_df$Name)
   }
-  else{
-    gg<-ggplot(data = dat.gg) +
-      geom_polygon_interactive(
-        aes(x, y, group = id,
-            tooltip = input_df$Element[id], data_id = id),
-        fill=color,
-        colour = "black",
-        show.legend = FALSE) +
-      scale_y_reverse() +
-      labs(title = "Hover over circle to display cluster elements") +
-      coord_equal()
-    ggiraph(ggobj = gg, width_svg = 5, height_svg = 5)
-  }
+  res<-ggiraph(ggobj = gg, width_svg = 5, height_svg = 5)
+  return(res)
 }
+
 
 
 # [END]
